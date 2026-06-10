94046a3d2a3d6577c57012ee5312c91733c636c4
rith-i-ka <24256.cs@rmkcet.ac.in>
2026-06-10 12:13:21 +0530

    added

--- FindingLengthOfSubstring.java
+++ FindingLengthOfSubstring.java
@@ -5,6 +5,7 @@ public class FindingLengthOfSubstring {
         String s1=sc.nextLine();
         String s2=sc.nextLine();
         int maxlen=0;
+        int endindex=0;
         int[][] substring=new int[s1.length()+1][s2.length()+1];
         for(int i=1;i<=s1.length();i++){
             for(int j=1;j<=s2.length();j++){
@@ -12,11 +13,16 @@ public class FindingLengthOfSubstring {
                     substring[i][j]=substring[i-1][j-1]+1;
                     if(substring[i][j]>maxlen){
                         maxlen=substring[i][j];
+                        endindex=i;
                     }
                 }
             }
         }
-        System.out.println(maxlen);
+        System.out.println("length : "+maxlen);
+        System.out.print("And the substring is : ");
+        for(int k=endindex-maxlen;k<endindex;k++){
+            System.out.print(s1.charAt(k));
+        }
 
     }
 }
